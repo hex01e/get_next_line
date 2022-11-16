b16a592e572a243ec96fb5e49e2afa634d3e3961
Hamza Oumansour <houmanso@e1r10p5.1337.ma>
2022-11-17 00:39:27 +0100

    about to done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: houmanso <houmanso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 19:58:29 by houmanso          #+#    #+#             */
-/*   Updated: 2022/11/17 00:13:03 by houmanso         ###   ########.fr       */
+/*   Updated: 2022/11/17 00:37:44 by houmanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,8 @@ static char	*get_the_line(char **str, char **backup, int i)
 	return (free(*str), line);
 }
 
-static char	*next_line(fd)
+static char	*next_line(int	fd, char **backup)
 {
-	static char	*backup;
 	char		*buff;
 	char		*str;
 	int			rd;
@@ -62,35 +61,39 @@ static char	*next_line(fd)
 	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buff)
 		return (NULL);
-	if (backup)
+	if (*backup)
 	{
-		str = ft_strjoin(str, backup);
-		free(backup);
-		backup = NULL;
+		str = ft_strjoin(str, *backup);
+		free(*backup);
+		*backup = NULL;
 		if ((i = is_a_newline(str)) != -1)
-			return (free(buff), get_the_line(&str, &backup, i));
+			return (free(buff), get_the_line(&str, backup, i));
 	}
 	rd = read(fd, buff, BUFFER_SIZE);
 	while (rd > 0)
 	{
 		str = ft_strjoin(str, buff);
 		if ((i = is_a_newline(str)) != -1)
-			return (free(buff), get_the_line(&str, &backup, i));
+			return (free(buff), get_the_line(&str, backup, i));
 		rd = read(fd, buff, BUFFER_SIZE);
 		buff[rd] = '\0';
 	}
 	if (rd < 0)
-		return (free(buff), free(str),free(backup) ,NULL);
-	if(rd == 0)return (free(buff),free(backup), str);
+		return (free(buff), free(str),free(*backup) ,NULL);
 	return (free(buff), str);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
+
+	static char	*backup;
+	char		*line;
+	
 	line = NULL;
+	if (backup && read(fd, NULL, 0) == -1)
+		return (free(backup), backup = NULL , NULL);
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
 		return (0);
-	line = next_line(fd);
+	line = next_line(fd, &backup);
 	return (line);
 }
--- main.c
+++ main.c
@@ -7,9 +7,8 @@ int main(int argc, char  *argv[])
 	int fd = open("tt",O_RDWR, 0644);
 
 	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
+	close(fd);
+	fd =  open("tt",O_RDWR, 0644);
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	return 0;
